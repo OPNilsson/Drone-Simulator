f819111eb0ee732a008922d6a21383edd1793357
the-xlk <79141812+the-xlk@users.noreply.github.com>
2021-10-13 17:27:15 +0200

    Added base UId
    3 Menus, main and one for each drone type. Dropdown menus, example calculated parameters, input parameters. Basic script to enable switching between menus.

---
+++ Assets/Scripts/UINavigator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UINavigator : MonoBehaviour
+{
+    public GameObject menuMain;
+    public GameObject menuDM;
+    public GameObject menuDB;
+
+    public void returnMain(){
+        menuDM.SetActive(false);
+        menuDB.SetActive(false);
+        menuMain.SetActive(true);
+    }
+
+    public void dM(){
+        menuDB.SetActive(false);
+        menuMain.SetActive(false);
+        menuDM.SetActive(true);
+    }
+
+    public void dB(){
+        menuMain.SetActive(false);
+        menuDM.SetActive(false);
+        menuDB.SetActive(true);
+    }
+}
