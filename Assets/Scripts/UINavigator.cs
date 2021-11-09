9f52532274d3597cd2a69cb460bbf2b5448083f0
the-xlk <79141812+the-xlk@users.noreply.github.com>
2021-11-09 22:52:35 +0100

    added buttons to update drone stats
    currently only sets them to certain values based on dropdowns. Should set input for calculations, calculate, then return the values.
    Currently only updates ui values

--- Assets/Scripts/UINavigator.cs
+++ Assets/Scripts/UINavigator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UINavigator : MonoBehaviour
 {
@@ -12,6 +13,18 @@ public class UINavigator : MonoBehaviour
     public Text MSx;
     public Text MSy;
     public Text MTS;
+    public GameObject DMmotor;
+    public GameObject DMbat;
+    public GameObject DMV;
+    public GameObject DMA;
+    public GameObject DMC;
+    public GameObject DMT;
+    public GameObject DBmotor;
+    public GameObject DBbat;
+    public GameObject DBV;
+    public GameObject DBA;
+    public GameObject DBC;
+    public GameObject DBT;
 
     public void dB()
     {
@@ -54,5 +67,83 @@ public class UINavigator : MonoBehaviour
         //mapRenderer.setSize((int)(Sx / TS), (int)(Sy / TS), TS);
 
         mapRenderer.setSize((int)Sx, (int)Sy, TS);
+    }
+    public void updateDM(){
+        float s=0,a=0,c=0,t=0;//should be made public and declared outside for external acces
+        //OR, could set their values from here...
+        switch(DMmotor.GetComponent<TMP_Dropdown>().value){
+            //idealy set input for stat calculation. Currently set output directly
+            case 0:
+                s=10;
+                a=1;
+                break;
+            case 1:
+                s=15;
+                a=2;
+                break;
+            default:
+                s=20;
+                a=3;
+                break;
+        }
+        switch(DMbat.GetComponent<TMP_Dropdown>().value){
+            //idealy set input for stat calculation. Currently set output directly
+            case 0:
+                c=10;
+                t=1000;
+                break;
+            case 1:
+                c=11;
+                t=1500;
+                break;
+            default:
+                c=12;
+                t=2000;
+                break;
+        }
+        //outputs "calculated", now set them
+        DMV.GetComponent<TextMeshProUGUI>().text=s.ToString();
+        DMA.GetComponent<TextMeshProUGUI>().text=a.ToString();
+        DMC.GetComponent<TextMeshProUGUI>().text=c.ToString();
+        DMT.GetComponent<TextMeshProUGUI>().text=t.ToString();
+    }
+    public void updateDB(){
+        float s=0,a=0,c=0,t=0;//should be made public and declared outside for external acces
+        //OR, could set their values from here...
+        switch(DBmotor.GetComponent<TMP_Dropdown>().value){
+            //idealy set input for stat calculation. Currently set output directly
+            case 0:
+                s=10;
+                a=1;
+                break;
+            case 1:
+                s=15;
+                a=2;
+                break;
+            default:
+                s=20;
+                a=3;
+                break;
+        }
+        switch(DBbat.GetComponent<TMP_Dropdown>().value){
+            //idealy set input for stat calculation. Currently set output directly
+            case 0:
+                c=10;
+                t=1000;
+                break;
+            case 1:
+                c=11;
+                t=1500;
+                break;
+            default:
+                c=12;
+                t=2000;
+                break;
+        }
+        //outputs "calculated", now set them
+        DBV.GetComponent<TextMeshProUGUI>().text=s.ToString();
+        DBA.GetComponent<TextMeshProUGUI>().text=a.ToString();
+        DBC.GetComponent<TextMeshProUGUI>().text=c.ToString();
+        DBT.GetComponent<TextMeshProUGUI>().text=t.ToString();
     }
 }
