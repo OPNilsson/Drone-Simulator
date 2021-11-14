55b8c3c615fa94525cb6ef98275d76675da825e4
Mads Finnerup <madsfinnerup@MacBook-Pro-tilhrende-Mads.local>
2021-11-14 18:03:53 +0100

    First iteration of the CSV writer, needs to be tested

---
+++ Assets/Scripts/ListToCSVConverter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using UnityEngine;
+
+public class ListToCSVConverter : MonoBehaviour
+{
+    string path = "/Users/madsfinnerup/desktop";
+    string path02 = Application.dataPath + "/Data/"  + "Saved_HumanData.csv";
+    public void ListToCSV(List<People> humanList)
+    {        
+        StreamWriter write = new StreamWriter(path02);
+
+        write.WriteLine("x-Cordinates, y-Cordinates, timeLeft");
+
+        foreach (People people in humanList) {
+            write.WriteLine(people.XCords + "," + people.YCords + "," + people.TimeToFind);
+        }
+        write.Flush();
+        write.Close();
+    }
+
+}
